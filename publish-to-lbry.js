d53f114bcda673b2d725626d886164b80579845a
Ryan Burgett <ryan@burgettdev.net>
2020-07-31 17:31:55 -0400

    Updated publish to lbry script

--- publish-to-lbry.js
+++ publish-to-lbry.js
@@ -148,7 +148,7 @@ const getConfirmations = async function(permanentUrl) {
 
       const params = {
         name,
-        bid: '0.01',
+        bid: '1.0',
         file_path: path.join(videosDir, `vv_${numberStr}.mp4`),
         validate_file: true,
         optimize_file: true,
